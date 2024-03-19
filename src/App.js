113602d7d4ad2c781bebd96a761929590b60fd83
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-19 13:15:18 +0300

    "slice added"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <Provider store={store}>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </Provider>
+    );
 };
 
 export default App;
